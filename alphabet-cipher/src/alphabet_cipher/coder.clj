5c6f34e8c1d417236d4e821a9f97e6baffc662e4
Andreas Werner <werner_andreas@web.de>
2015-01-05 19:57:50 +0100

    First version with straightforward implementation.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,30 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def alphabet (cycle "abcdefghijklmnopqrstuvwxyz"))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn- index
+  [c]
+  (- (int c) (int \a)))
+
+(defn- encode-char
+  "Encode the char c with key k"
+  [c k]
+  (nth (drop (index k) alphabet) (index c)))
+
+(defn encode [keyword message]
+  (apply str (map encode-char message (cycle keyword))))
+
+(defn- to-char [n]
+  (char (+ (int \a) n)))
+
+(defn- decode-char
+  "Decode the encoded char c with key k"
+  [c k]
+  (let [row (drop (index k) alphabet)]
+    (loop [index 0]
+      (if (= c (nth row index))
+        (to-char index)
+        (recur (inc index))))))
+
+(defn decode [keyword encoded-message]
+  (apply str (map decode-char encoded-message (cycle keyword))))

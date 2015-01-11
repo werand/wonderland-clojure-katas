e693625fadb9d43a2cea15671ce6fef267fd9c58
Andreas Werner <werner_andreas@web.de>
2015-01-11 20:46:45 +0100

    Refactoring für decode-char
    Removed loop, now calculating index.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,30 +1,29 @@
 (ns alphabet-cipher.coder)
 
 (def alphabet (cycle "abcdefghijklmnopqrstuvwxyz"))
+(def alphabet-length 26)
 
 (defn- index [c] (- (int c) (int \a)))
+
+(defn- find-row [k] (drop (index k) alphabet))
 
 (defn- encode-char
   "Encode the char c with key k"
   [c k]
-  (nth (drop (index k) alphabet) (index c)))
+  (nth (find-row k) (index c)))
+
+(defn- decode-char
+  "Decode the encoded char c with key k"
+  [c k]
+  (let [row-first-char-index (index (first (find-row k)))
+        c-index (+ (- (index c) row-first-char-index) alphabet-length)]
+    (nth alphabet c-index)))
 
 (defn- transcode [f keyword message]
   (apply str (map f message (cycle keyword))))
 
 (defn encode [keyword message]
   (transcode encode-char keyword message))
-
-(defn- to-char [n] (char (+ (int \a) n)))
-
-(defn- decode-char
-  "Decode the encoded char c with key k"
-  [c k]
-  (let [row (drop (index k) alphabet)]
-    (loop [index 0]
-      (if (= c (nth row index))
-        (to-char index)
-        (recur (inc index))))))
 
 (defn decode [keyword encoded-message]
   (transcode decode-char keyword encoded-message))

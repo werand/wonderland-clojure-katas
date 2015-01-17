c077e7ed6034af6d8c4ba6d77ff24552b65bf17c
Andreas Werner <werner_andreas@web.de>
2015-01-17 17:42:51 +0100

    Initial n-von-k function.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,16 @@
 (ns wonderland-number.finder)
 
+(defn n-von-k
+  ([s n]
+     (n-von-k s n []) )
+  ([s n acc]
+     (if (> n 0)
+       (if (seq s)
+         (map (fn [x]
+                (n-von-k (remove #(= x %) s) (dec n) (cons x acc)))
+              s))
+       acc)))
+
 (defn wonderland-number []
   ;; calculate me
   42)

4ccf1c1883e9eba54319a63a1fe531db16b98d89
Andreas Werner <werner_andreas@web.de>
2015-01-25 15:39:02 +0100

    Final touch.

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,6 +1,6 @@
 (ns tiny-maze.solver)
 
-(defn find-position
+(defn- find-position
   [keyword maze]
   (loop [maze maze row (first maze) row-n 0 col-n 0]
     (if (seq maze)
@@ -15,24 +15,20 @@
 
 (def find-end (partial find-position :E))
 
-(defn next-positions
+(defn- next-positions
   [pos]
-  (let [north [-1 0]
-        south [1 0]
-        east [0 -1]
-        west [0 1]]
-    (map #(map + pos %) [west south east north])))
+  (map #(map + pos %) [[-1 0] [1 0] [0 -1] [0 1]]))
 
-(defn wall?
+(defn- path?
   [maze pos]
   (let [v (get-in maze pos)]
     (and v (not= 1 v))))
 
-(defn filter-possible-directions
+(defn- filter-possible-directions
   [maze next-positions]
-  (filter (partial wall? maze) next-positions))
+  (filter (partial path? maze) next-positions))
 
-(defn draw-path
+(defn- draw-path
   [maze path]
   (let [x (fn [_] :x)]
     (loop [path path solution maze]
@@ -40,20 +36,22 @@
         (recur (rest path) (update-in solution (first path) x))
         solution))))
 
+(defn- find-solutions
+  [maze end-pos pos path visited]
+  (when-not (contains? visited pos)
+    (let [next-pos (filter-possible-directions maze (next-positions pos))]
+      (if-not (some #{end-pos} next-pos)
+        (mapcat #(find-solutions maze end-pos % (cons pos path) (conj visited pos)) next-pos)
+        [(cons pos path)]))))
+
 (defn solve-maze
-  ([maze]
-     (let [start-pos (find-start maze)
-           end-pos (find-end maze)
-           solutions (solve-maze maze end-pos start-pos [] #{})]
-       (when (seq solutions)
-        (->> solutions
-              (sort-by count)
-              first
-              (cons end-pos)
-              (draw-path maze)))))
-  ([maze end-pos pos path visited]
-     (when-not (contains? visited pos)
-       (let [next-pos (filter-possible-directions maze (next-positions pos))]
-         (if-not (some #{end-pos} next-pos)
-           (mapcat #(solve-maze maze end-pos % (cons pos path) (conj visited pos)) next-pos)
-           [(cons pos path)])))))
+  [maze]
+  (let [start-pos (find-start maze)
+        end-pos (find-end maze)
+        solutions (find-solutions maze end-pos start-pos [] #{})]
+    (when (seq solutions)
+      (->> solutions
+           (sort-by count)
+           first
+           (cons end-pos)
+           (draw-path maze)))))
